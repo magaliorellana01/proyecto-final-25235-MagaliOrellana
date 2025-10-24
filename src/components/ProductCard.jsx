b3dae850e76ece9b94e2cf3d48f11957af26a995
magaliorellana01 <magaliorellana01@gmail.com>
2025-10-24 03:15:56 -0300

    Correccion de los botones de 'Agregar al Carrito

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -20,7 +20,15 @@ const ProductCard = ({ producto, agregarAlCarrito }) => {
                 <Card.Text>
                     <strong>${producto.price}</strong>
                 </Card.Text>
-                <Button style = {{backgroundColor: "#333D29" }} onClick={() => agregarAlCarrito(producto)}>
+                <Button style={{ backgroundColor: "#333D29", borderColor: "#333D29", transition: "all 0.3s" }}
+                    onMouseEnter={e => {
+                        e.currentTarget.style.backgroundColor = "#C2C5AA";
+                        e.currentTarget.style.color = "#333D29";
+                    }}
+                    onMouseLeave={e => {
+                        e.currentTarget.style.backgroundColor = "#333D29";
+                        e.currentTarget.style.color = "#C2C5AA";
+                    }} onClick={() => agregarAlCarrito(producto)}>
                     Agregar al carrito
                 </Button>
             </Card.Body>
