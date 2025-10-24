38342f798f6fa9dbd2b7ba598f70cdac5d1e3a7c
magaliorellana01 <magaliorellana01@gmail.com>
2025-10-24 02:32:03 -0300

    Componente Productos agregado

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,1 +1,12 @@
+import React from "react";
+import ListaProductos from "./ListaProductos";
 
+export default function Productos() {
+  return (
+    <div className="container">
+      <h1 style={{ color: "#333D29" }} > PRODUCTOS </h1>
+      <ProductList category="jewelery" />
+    </div>
+
+  );
+};
